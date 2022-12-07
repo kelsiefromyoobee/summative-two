50f194cfdc65e56fe75a71dcdce7ddc72b714063
kelsiefromyoobee <kelsie.baigent@gmail.com>
2022-12-07 13:48:52 +1300

    SO over this JAVA CRIPS not WORKING

--- resources/script/script.js
+++ resources/script/script.js
@@ -11,7 +11,15 @@ console.log( user_input_for_people );
 let check_in_input = document.getElementById('check-in').value;
 let check_out_input = document.getElementById('check-out').value
 
-console.log( check_in_input );
-console.log( check_out_input );
+// datecalculator.js
+let difference = calculate_day_difference(check_in_input , check_out_input)
+console.log(difference);
 
+// IF Hotel Statement
+
+if (user_input_for_people >=2 && difference >=5)
+    document.getElementsByClassName('hotel').classList.remove('disabled')
+    else
+    document.getElementsByClassName('hotel').classList.add('disabled')
+    console.log(true);
 }
