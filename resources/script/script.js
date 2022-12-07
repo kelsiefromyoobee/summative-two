e46030f8fbf9a4e01af08dabbc4a342417d619c0
kelsiefromyoobee <kelsie.baigent@gmail.com>
2022-12-08 12:03:02 +1300

    added java

--- resources/script/script.js
+++ resources/script/script.js
@@ -16,10 +16,32 @@ let difference = calculate_day_difference(check_in_input , check_out_input)
 console.log(difference);
 
 // IF Hotel Statement
+if (user_input_for_people >= 1 /*min people for hotel*/ && user_input_for_people <= 2 && difference >= 1 && difference <= 5)
+    document.getElementById('hotel').classList.remove('disabled')
+else
+    document.getElementById('hotel').classList.add('disabled')
+    
+// IF Hostel Statement
+if (user_input_for_people ==1 && difference >= 1 && difference <= 10)
+    document.getElementById('hostel').classList.remove('disabled')
+else
+    document.getElementById('hostel').classList.add('disabled')
 
-if (user_input_for_people >=2 && difference >=5)
-    document.getElementsByClassName('hotel').classList.remove('disabled')
-    else
-    document.getElementsByClassName('hotel').classList.add('disabled')
-    console.log(true);
+// IF Motel
+if (user_input_for_people >= 2 && user_input_for_people <= 4 && difference >= 3 && difference <=10)
+    document.getElementById('motel').classList.remove('disabled')
+else
+    document.getElementById('motel').classList.add('disabled')
+
+// If House
+if (user_input_for_people >=1 && user_input_for_people <= 4 && difference >= 2 && difference <= 15)
+    document.getElementById('house').classList.remove('disabled')
+else
+    document.getElementById('house').classList.add('disabled')
 }
+
+//TO DO
+// add HOVER Modal
+// add on click modal
+// food database
+
